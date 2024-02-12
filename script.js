b93829b4ff171296e44fd851b3b842afa7c4255a
Pankaj Singh Negi <157361681+Pankaj-VS@users.noreply.github.com>
2024-02-12 15:35:17 +0530

    added individual delete button for each task

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Selecting task list once and reusing it
 const taskList = document.getElementById('taskList');
 
-// marking tasks as completed
+// marking tasks as completed.
 taskList.addEventListener('click', function(event) {
     const listItem = event.target.closest('li');
     if (listItem) {
@@ -9,29 +8,43 @@ taskList.addEventListener('click', function(event) {
     }
 });
 
-// deleting completed tasks
- document.getElementById('deleteCompletedButton').addEventListener('click', function(){
+// deleting completed tasks.
+document.getElementById('deleteCompletedButton').addEventListener('click', function() {
     const completedTasks = document.querySelectorAll('.completed');
     completedTasks.forEach(task => {
         task.remove();
     });
 });
 
+// Deleting individual tasks.
+taskList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('deleteButton')) {
+        event.target.parentNode.remove(); 
+    }
+});
 
-// To add document by either pressing the enter button or by clicking on the add task button.
+
+
+
+document.getElementById('addButton').addEventListener('click', addTask);
+
 document.getElementById('task').addEventListener('keypress', function(event) {
     if (event.key === 'Enter') {
         addTask();
     }
 });
-document.getElementById('addButton').addEventListener('click', addTask);
 
+// Adding tasks.
 function addTask() {
     let taskInput = document.getElementById('task').value;
     
     if (taskInput.trim() !== '') {
         let newTask = document.createElement('li');
-        newTask.appendChild(document.createTextNode(taskInput));
+        newTask.classList.add('task-item');
+        newTask.innerHTML = `
+            <span>${taskInput}</span>
+            <button class="deleteButton">Delete</button>
+        `;
         taskList.appendChild(newTask);
         taskInput = '';
     } else {
